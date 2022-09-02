b9ec470f858147c935509de02a7ee519580dca72
michimani <michimani210@gmail.com>
2022-09-02 22:01:21 +0900

    update: hugo version v0.102.3

--- aws__codebuild__project.tf
+++ aws__codebuild__project.tf
@@ -110,7 +110,7 @@ resource "aws_codebuild_project" "hugo_build_project" {
     phases:
       install:
         commands:
-          - curl -Ls https://github.com/gohugoio/hugo/releases/download/v0.79.0/hugo_0.79.0_Linux-64bit.tar.gz -o /tmp/hugo.tar.gz
+          - curl -Ls https://github.com/gohugoio/hugo/releases/download/v0.102.3/hugo_0.102.3_Linux-64bit.tar.gz -o /tmp/hugo.tar.gz
           - tar xf /tmp/hugo.tar.gz -C /tmp
           - mv /tmp/hugo /usr/bin/hugo
           - rm -rf /tmp/hugo*
